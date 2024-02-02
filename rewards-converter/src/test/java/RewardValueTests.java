1acbfda96e90e9dc59f5f21fadd2643bad4e1c9a
keyurpatel905324 <157721106+keyurpatel905324@users.noreply.github.com>
2024-02-01 19:28:06 -0500

    Add files via upload

---
+++ rewards-converter/src/test/java/RewardValueTests.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RewardValueTests {
+
+    @Test
+    void convert_from_cash_to_miles() {
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue / 0.0035; // Conversation rate
+        assertEquals(expectedMiles, rewardValue.getMilesValue());
+    }
+
+    @Test
+    void convert_from_miles_to_cash() {
+        int milesValue = 10000;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCash = milesValue * 0.0035;//Conversation rate
+        assertEquals(expectedCash, rewardValue.getCashValue());
+    }
+}
